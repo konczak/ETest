758c143540f8a88e21f113a2af1507aad2f99036
Piotr Konczak <konczak.piotrek@gmail.com>
2014-02-23 23:02:19 +0100

    add REST style validation errors preparing response solution

---
+++ src/main/java/pl/konczak/etest/assembler/ValidationErrorAssembler.java
@@ -0,0 +1,53 @@
+package pl.konczak.etest.assembler;
+
+import java.util.List;
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import pl.konczak.etest.dto.ValidationErrorDTO;
+
+@Component
+public class ValidationErrorAssembler {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public ValidationErrorAssembler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    public ValidationErrorDTO toValidationErrorDTO(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        return processFieldErrors(fieldErrors);
+    }
+
+    private ValidationErrorDTO processFieldErrors(List<FieldError> fieldErrors) {
+        ValidationErrorDTO dto = new ValidationErrorDTO();
+
+        for (FieldError fieldError : fieldErrors) {
+            String localizedErrorMessage = resolveLocalizedErrorMessage(fieldError);
+            dto.addFieldError(fieldError.getField(), localizedErrorMessage);
+        }
+
+        return dto;
+    }
+
+    private String resolveLocalizedErrorMessage(FieldError fieldError) {
+        Locale currentLocale = LocaleContextHolder.getLocale();
+        String localizedErrorMessage = messageSource.getMessage(fieldError, currentLocale);
+
+        if (localizedErrorMessage.equals(fieldError.getDefaultMessage())) {
+            String[] fieldErrorCodes = fieldError.getCodes();
+            localizedErrorMessage = fieldErrorCodes[0];
+        }
+
+        return localizedErrorMessage;
+    }
+}
---
+++ src/main/java/pl/konczak/etest/dto/ValidationErrorDTO.java
@@ -0,0 +1,21 @@
+package pl.konczak.etest.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErrorDTO {
+
+    private List<FieldErrorDTO> fieldErrors = new ArrayList<FieldErrorDTO>();
+
+    public ValidationErrorDTO() {
+    }
+
+    public List<FieldErrorDTO> getFieldErrors() {
+        return fieldErrors;
+    }
+
+    public void addFieldError(String path, String message) {
+        FieldErrorDTO fieldErrorDTO = new FieldErrorDTO(path, message);
+        this.fieldErrors.add(fieldErrorDTO);
+    }
+}
